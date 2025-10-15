08224046569df4be0c56258d1934c8e7ed0c3222
Tim Kennedy <tim.kennedy6@yahoo.com>
2025-10-15 16:05:47 -0400

    Updated shop location screen

--- screens/Location.js
+++ screens/Location.js
@@ -1,10 +1,21 @@
-import { Text, View } from 'react-native';
+import { Text, View, ImageBackground } from 'react-native';
+import BarberChair from '../assets/BarberChair.jpg';
+import {Card} from 'react-native-elements';
 
 const Location = () => {
     return (
-        <View>
-            <Text>DD is a mobile barber moving into a new shop soon!</Text>
-        </View>
+        <ImageBackground
+              source={BarberChair}
+              style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+              resizeMode="cover"
+            >
+              <Card containerStyle={{ backgroundColor: "rgba(210, 52, 155, 0.8)" }}>
+                <Text>Did this do anything?</Text>
+              </Card>
+               <Card containerStyle={{ backgroundColor: "rgba(210, 52, 155, 0.8)" }}>
+                <Text>Did this do anything?</Text>
+              </Card>
+            </ImageBackground>
     )
 };
 
